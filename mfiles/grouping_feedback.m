0c148d9172bae74ec228644b7c5e2c406ff8dece
julianZeitler <julian.zeitler@zeiss.com>
2024-09-16 16:05:41 +0200

    Bugfix: Wrong inhibitory input

--- mfiles/grouping_feedback.m
+++ mfiles/grouping_feedback.m
@@ -30,14 +30,14 @@ for l = bPrs.minLevel:bPrs.maxLevel
         % bPyr1_1(l).orientation(ori).data = cPyr(l).orientation(ori).data .* (1+bPrs.alpha.*(1./(1+exp(-((1/1)*(vmL1(l).orientation(ori).data-vmD2(l).orientation(ori).data))))));
         P1_1 = imfilter(cPyr(l).orientation(ori).data, params.bPrs.FF.spatial_neighborhood); % Perisomatic Input (FF)
         D1_1 = params.bPrs.FB.scale * bPyr1_1(l).orientation(ori).data .* (1./(1+exp(-(vmL1(l).orientation(ori).data - params.bPrs.FB.offset)))); % Distal Input (FB)
-        Norm1_1 = params.bPrs.exp_decay + params.bPrs.FF.inhibition * imfilter(cPyr(l).orientation(ori).data, params.bPrs.FB.spatial_neighborhood) + params.bPrs.FB.inhibition * imfilter(bPyr1_1(l).orientation(ori).data, params.bPrs.FB.spatial_neighborhood); % Denominator
+        Norm1_1 = params.bPrs.exp_decay + params.bPrs.FF.inhibition * imfilter(cPyr(l).orientation(ori).data, params.bPrs.FB.spatial_neighborhood) + params.bPrs.FB.inhibition * imfilter(bPyr1_2(l).orientation(ori).data, params.bPrs.FB.spatial_neighborhood); % Denominator
         bPyr1_1(l).orientation(ori).data = (P1_1 + D1_1)./Norm1_1;
         bPyr1_1(l).orientation(ori).data(bPyr1_1(l).orientation(ori).data<0 | isnan(bPyr1_1(l).orientation(ori).data)) = 0; % set invalid data to 0
 
         % bPyr1_2(l).orientation(ori).data = cPyr(l).orientation(ori+8).data .* (1+bPrs.alpha.*(1./(1+exp(-((1/1)*(vmL2(l).orientation(ori).data-vmD1(l).orientation(ori).data))))));
         P1_2 = imfilter(cPyr(l).orientation(ori+8).data, params.bPrs.FF.spatial_neighborhood); % Perisomatic Input
         D1_2 = params.bPrs.FB.scale * bPyr1_2(l).orientation(ori).data .* (1./(1+exp(-(vmL2(l).orientation(ori).data - params.bPrs.FB.offset)))); % Distal Input
-        Norm1_2 = params.bPrs.exp_decay + params.bPrs.FF.inhibition * imfilter(cPyr(l).orientation(ori+8).data, params.bPrs.FB.spatial_neighborhood) + params.bPrs.FB.inhibition * imfilter(bPyr1_2(l).orientation(ori).data, params.bPrs.FB.spatial_neighborhood); % Denominator
+        Norm1_2 = params.bPrs.exp_decay + params.bPrs.FF.inhibition * imfilter(cPyr(l).orientation(ori+8).data, params.bPrs.FB.spatial_neighborhood) + params.bPrs.FB.inhibition * imfilter(bPyr1_1(l).orientation(ori).data, params.bPrs.FB.spatial_neighborhood); % Denominator
         bPyr1_2(l).orientation(ori).data = (P1_2 + D1_2)./Norm1_2;
         bPyr1_2(l).orientation(ori).data(bPyr1_2(l).orientation(ori).data<0 | isnan(bPyr1_2(l).orientation(ori).data)) = 0;
 
@@ -45,14 +45,14 @@ for l = bPrs.minLevel:bPrs.maxLevel
         % bPyr2_1(l).orientation(ori).data = cPyr(l).orientation(ori+8).data .* (1+bPrs.alpha.*(1./(1+exp(-((1/1)*(vmD1(l).orientation(ori).data-vmL2(l).orientation(ori).data))))));
         P2_1 = imfilter(cPyr(l).orientation(ori+8).data, params.bPrs.FF.spatial_neighborhood); % Perisomatic Input
         D2_1 = params.bPrs.FB.scale * bPyr2_1(l).orientation(ori).data .* (1./(1+exp(-(vmD1(l).orientation(ori).data - params.bPrs.FB.offset)))); % Distal Input
-        Norm2_1 = params.bPrs.exp_decay + params.bPrs.FF.inhibition * imfilter(cPyr(l).orientation(ori+8).data, params.bPrs.FB.spatial_neighborhood) + params.bPrs.FB.inhibition * imfilter(bPyr2_1(l).orientation(ori).data, params.bPrs.FB.spatial_neighborhood); % Denominator
+        Norm2_1 = params.bPrs.exp_decay + params.bPrs.FF.inhibition * imfilter(cPyr(l).orientation(ori+8).data, params.bPrs.FB.spatial_neighborhood) + params.bPrs.FB.inhibition * imfilter(bPyr2_2(l).orientation(ori).data, params.bPrs.FB.spatial_neighborhood); % Denominator
         bPyr2_1(l).orientation(ori).data = (P2_1 + D2_1)./Norm2_1;
         bPyr2_1(l).orientation(ori).data(bPyr2_1(l).orientation(ori).data<0 | isnan(bPyr2_1(l).orientation(ori).data)) = 0;
 
         % bPyr2_2(l).orientation(ori).data = cPyr(l).orientation(ori).data .* (1+bPrs.alpha.*(1./(1+exp(-((1/1)*(vmD2(l).orientation(ori).data-vmL1(l).orientation(ori).data))))));
         P2_2 = imfilter(cPyr(l).orientation(ori).data, params.bPrs.FF.spatial_neighborhood); % Perisomatic Input
         D2_2 = params.bPrs.FB.scale * bPyr2_2(l).orientation(ori).data .* (1./(1+exp(-(vmD2(l).orientation(ori).data - params.bPrs.FB.offset)))); % Distal Input
-        Norm2_2 = params.bPrs.exp_decay + params.bPrs.FF.inhibition * imfilter(cPyr(l).orientation(ori).data, params.bPrs.FB.spatial_neighborhood) + params.bPrs.FB.inhibition * imfilter(bPyr2_2(l).orientation(ori).data, params.bPrs.FB.spatial_neighborhood); % Denominator
+        Norm2_2 = params.bPrs.exp_decay + params.bPrs.FF.inhibition * imfilter(cPyr(l).orientation(ori).data, params.bPrs.FB.spatial_neighborhood) + params.bPrs.FB.inhibition * imfilter(bPyr2_1(l).orientation(ori).data, params.bPrs.FB.spatial_neighborhood); % Denominator
         bPyr2_2(l).orientation(ori).data = (P2_2 + D2_2)./Norm2_2;
         bPyr2_2(l).orientation(ori).data(bPyr2_2(l).orientation(ori).data<0 | isnan(bPyr2_2(l).orientation(ori).data)) = 0;
 
