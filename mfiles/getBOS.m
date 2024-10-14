0c11f7386372228c87d30fa45a61e95248f741c1
julianZeitler <julian.zeitler@zeiss.com>
2024-10-14 08:53:03 +0200

    Updated BOS-calculation
    Instead of only taking the maximum value for BOS-orientation, sum all BOS-values interpreted as vectors.

--- mfiles/getBOS.m
+++ mfiles/getBOS.m
@@ -12,6 +12,7 @@ function [BOS] = getBOS(B1, B2, params, norm)
 %           H - range [0,1] (encodes [0,2pi]) -> BOS-direction
 %           S - normalized strength of BOS-Signal
 %           V - 1
+
 norm_flag = true;
 if nargin > 3
     if norm=="normalized"
@@ -26,33 +27,16 @@ BOS = zeros(dimensions(1), dimensions(2), 3);
 
 for i=1:dimensions(1)
     for j=1:dimensions(2)
-        % for each pixel, get the maximum BOS-Value. This direction will be
-        % used to calculate the H-Value
-        [max_val,max_idx] = max(B1(:,i,j)-B2(:,i,j));
-        [min_val,min_idx] = min(B1(:,i,j)-B2(:,i,j));
-        if abs(max_val) > abs(min_val)
-            % BOS-direction is -90° to edge orientation
-            theta1 = params.oris(max_idx)-pi/2;
-            if theta1 < 0
-                theta1 = theta1 + 2*pi;
-            elseif theta1 > 2*pi
-                theta1 = theta1 - 2*pi;
-            end
-            BOS(i,j,1) = (theta1)/(2*pi); % Normalize to [0,1]
-            BOS(i,j,2) = abs(max_val);
-            BOS(i,j,3) = 1;
-        else
-            % BOS-direction is +90° to edge orientation
-            theta2 = params.oris(min_idx)+pi/2;
-            if theta2 < 0
-                theta2 = theta2 + 2*pi;
-            elseif theta2 > 2*pi
-                theta2 = theta2 - 2*pi;
-            end
-            BOS(i,j,1) = (theta2)/(2*pi); % Normalize to [0,1]
-            BOS(i,j,2) = abs(min_val);
-            BOS(i,j,3) = 1;
-        end
+        % for each pixel, sum the BOS-vectors represented by their
+        % orientation (edge ori +/- pi/2) and length (BOS-strength)
+        % Rotate oris +90° to go from edge orientation to BOS-orientation.
+        [X, Y] = pol2cart(params.oris+pi/2, (B1(:,i,j)-B2(:,i,j)).');
+        [theta, rho] = cart2pol(sum(X), sum(Y)); % Perform addition in cartesian coordinates
+        theta = wrapTo2Pi(theta);
+
+        BOS(i,j,1) = theta/(2*pi); % Normalize to [0,1]
+        BOS(i,j,2) = rho;
+        BOS(i,j,3) = 1;
 
     end
 end
